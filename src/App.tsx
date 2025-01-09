38409d2ad4e2fe7af83d695cc953d8c176a2295a
김진호 <rlawlsgh1227@gmail.com>
2025-01-09 11:55:07 +0900

    chore: query client provider 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 0,
+      staleTime: 1000 * 5,
+      gcTime: 1000 * 5,
+    },
+  },
+});
+
 function App() {
-  return <h1>Self-Paced React</h1>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <h1>Self-Paced React</h1>;
+    </QueryClientProvider>
+  );
 }
 
 export default App;
