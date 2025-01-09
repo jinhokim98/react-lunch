22ca8536ebc2fd4c5b6c8e5d0d5b15b573c3da55
김진호 <rlawlsgh1227@gmail.com>
2025-01-09 11:54:53 +0900

    style: lint 스타일 설정 및 적용

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: {browser: true, es2020: true},
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
@@ -10,16 +10,13 @@ module.exports = {
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
+  parserOptions: {ecmaVersion: 'latest', sourceType: 'module'},
+  settings: {react: {version: '18.2'}},
   plugins: ['react-refresh', '@typescript-eslint'],
   rules: {
     'react/jsx-no-target-blank': 'off',
     'react/prop-types': 'off',
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    'react-refresh/only-export-components': ['warn', {allowConstantExport: true}],
     '@typescript-eslint/no-unused-vars': ['error'],
   },
 };
--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 
 async function enableMocking() {
-  const { worker } = await import('./mocks/browser.js');
+  const {worker} = await import('./mocks/browser.js');
 
   return worker.start();
 }
@@ -12,6 +12,6 @@ enableMocking().then(() => {
   ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
       <App />
-    </React.StrictMode>
+    </React.StrictMode>,
   );
 });
