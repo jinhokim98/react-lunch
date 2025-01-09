a43421a1370f8899efd32438944af5c87fd9be1a
김진호 <rlawlsgh1227@gmail.com>
2025-01-09 12:28:31 +0900

    feat: restaurant 컴포넌트 생성

---
+++ src/components/RestaurantList/index.tsx
@@ -0,0 +1,9 @@
+import styles from './style.module.css';
+
+export const RestaurantList = () => {
+  return (
+    <section className={styles.restaurantListContainer}>
+      <ul className="restaurant-list"></ul>
+    </section>
+  );
+};
