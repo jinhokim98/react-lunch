9b9d3888bf3ee1a4e7d09d25c0a903fbebb2301f
김진호 <rlawlsgh1227@gmail.com>
2025-01-09 22:25:37 +0900

    chore: 배포 관련 설정

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,17 @@ import App from './App.tsx';
 async function enableMocking() {
   const {worker} = await import('./mocks/browser.js');
 
-  return worker.start();
+  if (import.meta.env.MODE === 'development') {
+    // 개발 환경에서는 기본 경로 사용
+    return worker.start();
+  } else if (import.meta.env.MODE === 'production') {
+    // 프로덕션 환경에서는 GitHub Pages 경로에 맞게 설정
+    return worker.start({
+      serviceWorker: {
+        url: '/react-lunch/mockServiceWorker.js', // GitHub Pages 경로
+      },
+    });
+  }
 }
 
 enableMocking().then(() => {
