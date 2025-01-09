a2cedf2a3e82320e4f182d5bee057e8908022e34
김진호 <rlawlsgh1227@gmail.com>
2025-01-09 12:43:16 +0900

    feat: 버튼 컴포넌트 생성

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,22 @@
+import styles from './style.module.css';
+
+type ButtonVariants = 'primary' | 'secondary';
+
+type ButtonProps = React.ComponentProps<'button'> & {
+  variants: ButtonVariants;
+};
+
+const BUTTON_STYLE: Record<ButtonVariants, string> = {
+  primary: styles.buttonPrimary,
+  secondary: styles.buttonSecondary,
+};
+
+export const Button = ({children, variants, ...buttonProps}: ButtonProps) => {
+  return (
+    <div className={styles.buttonContainer}>
+      <button className={`${styles.button} ${BUTTON_STYLE[variants]} text-caption`} {...buttonProps}>
+        {children}
+      </button>
+    </div>
+  );
+};
