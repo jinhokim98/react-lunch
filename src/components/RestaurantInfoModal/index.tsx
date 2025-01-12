1b95d74a06d36c47d1e6199c5b02f9f5136dc0b5
김진호 <rlawlsgh1227@gmail.com>
2025-01-12 12:01:16 +0900

    feat: 음식점 상세 정보 수정

--- src/components/RestaurantInfoModal/index.tsx
+++ src/components/RestaurantInfoModal/index.tsx
@@ -1,20 +1,31 @@
 import {MODAL_NAME} from '../../constants/modal';
 import {useRestaurantStore} from '../../store/restaurants';
 import {Modal} from '../Modal';
+import {RestaurantImage} from '../RestaurantImage';
 import styles from './style.module.css';
 
 export const RestaurantInfoModal = () => {
   const {selectedRestaurant, selectRestaurant} = useRestaurantStore();
 
+  if (!selectedRestaurant) {
+    return null;
+  }
+
   return (
     <Modal
       name={MODAL_NAME.info}
-      title={selectedRestaurant?.name ?? '음식점 정보'}
+      title={selectedRestaurant.name}
       buttonName="닫기"
       button={{onClick: () => selectRestaurant(null)}}
     >
       <div className={styles.restaurantInfo}>
-        <p className="restaurant-info__description text-body">{selectedRestaurant?.description}</p>
+        <RestaurantImage category={selectedRestaurant.category} />
+        <h2 className="text-subtitle">{selectedRestaurant.name}</h2>
+        <h3 className={`${styles.restaurantDistance} text-body`}>{`캠퍼스부터 ${selectedRestaurant.distance}분 내`}</h3>
+        <p className={`${styles.restaurantDescription} text-body`}>{selectedRestaurant.description}</p>
+        <a href={selectedRestaurant.link} target="__blank__" className={`${styles.restaurantLink} text-body`}>
+          {selectedRestaurant.link}
+        </a>
       </div>
     </Modal>
   );
